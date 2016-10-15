580d843421b7a7f688a1432eccd3a8d28281604b
xukun <xukunironman@gmail.com>
2016-10-15 17:52:00 +0800

    debug,and now can be displayede correctly in broswser

--- web_we/bin/app.py
+++ web_we/bin/app.py
@@ -1,14 +1,14 @@
 import web
 
-urls=(
-	'/','Home'
-	
-)
-render=web.template.render('templates')
+urls=('/',"home")
+render=web.template.render('templates/')
+
+
 class home:
 	def GET(self):
-		return render.Home()
+		return render.home()
 	
 if __name__=="__main__":
-	app.run()
+    app=web.application(urls,globals())
+    app.run()
 	
