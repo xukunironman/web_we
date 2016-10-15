490b3df07ba67da002755ea952a118983ba6473c
xukun <xukunironman@gmail.com>
2016-10-15 18:42:04 +0800

    create photo class in app.py

--- web_we/bin/app.py
+++ web_we/bin/app.py
@@ -1,13 +1,18 @@
 import web
 
-urls=('/',"home")
+urls=(
+'/',"home",
+'/photo','photo')
 render=web.template.render('templates/')
 
 
 class home:
 	def GET(self):
 		return render.home()
-	
+
+class photo:
+	def GET(self):
+		return render.photo()
 if __name__=="__main__":
     app=web.application(urls,globals())
     app.run()
